3b3ff26c0714e10105356589b90032c9519ccfc7
Prajakt Shastry <prajakt.shastry@gmail.com>
2019-11-22 03:29:27 -0800

    Adding more software

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -33,3 +33,27 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add 
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt update
 sudo apt install -y mongodb-org
+
+echo "Installing npm"
+
+
+echo "Install eslint"
+
+npm install -g eslint
+
+echo "Installing mongodb compass"
+
+wget https://downloads.mongodb.com/compass/mongodb-compass_1.15.1_amd64.deb
+sudo dpkg -i mongodb-compass_1.15.1_amd64.deb
+
+echo "Installing angular cli 8"
+npm install -g @angular/cli@8
+
+echo "Installing react cli"
+npm install -g react@16
+
+echo "Installing http server"
+npm install http-server -g
+
+echo "Installing postman"
+sudo snap install postman
